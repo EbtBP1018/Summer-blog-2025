3b2679edc8ac87fe65c05466b5f51544fa876120
Ella BP <jakepuder110@gmail.com>
2025-05-31 14:47:35 -0700

    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = "Ella";
+let age = 15;
+let likesSushi = true;
+
+function greeting(name) {
+    console.log("hello", name);
+    }
+
+greeting("Ella");
+
+let newName = document.getElementByID("greeter").value;
